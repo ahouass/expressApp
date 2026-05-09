a5886c8ca03767ae4da0e81c9f445664e2c299ac
Aiman Houass <aiman.1337.houass@gmail.com>
2026-05-09 18:04:57 +0000

    done

--- api/index.js
+++ api/index.js
@@ -34,7 +34,4 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
-})
+module.exports = app
