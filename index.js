f6a5b0a1b188c6d22c116841a9a46a5a26533a51
Aiman Houass <aiman.1337.houass@gmail.com>
2026-05-10 10:55:21 +0000

    done

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
